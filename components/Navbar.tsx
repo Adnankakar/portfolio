7d89d4df1bfeb14a6bcbe308aa13d883c4ea5fe8
adnan kakar <eradnan632@gmail.com>
2026-08-12 15:21:08 +0530

    Remove experience section

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ const navLinks = [
   { name: "Home", href: "#home" },
   { name: "About", href: "#about" },
   { name: "Skills", href: "#skills" },
-  { name: "Experience", href: "#experience" },
+  // { name: "Experience", href: "#experience" },
   { name: "Projects", href: "#projects" },
   { name: "Education", href: "#education" },
   { name: "Contact", href: "#contact" },
